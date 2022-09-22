599566ca99685a00d0e587553ba9d1a414b4cd34
Inaam2 <inaam.mohammed29@gmail.com>
2022-09-21 22:40:02 -0400

    c

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -51,7 +51,7 @@ class CrossbodyBag extends Bag {
 
     @Override
     public String toString() {
-        return super.getColor() + " Bag  with "  + this.numberOfStraps + " (" + super.getNumberOfContents() + " / " +
+        return super.getColor() + " Crossbody Bag  with "  + this.numberOfStraps + " (" + super.getNumberOfContents() + " / " +
                 super.getCapacity() + ")";
 
     }
